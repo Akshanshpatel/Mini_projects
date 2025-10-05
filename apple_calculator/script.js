cf86b21a19176af0a1aff877676e67fde9e73de0
Akshansh Patel <akshanshpatel003@gmail.com>
2025-10-05 14:03:28 +0530

    Showcase

---
+++ apple_calculator/script.js
@@ -0,0 +1,63 @@
+let buttons=document.querySelectorAll('button')
+let input=document.querySelector('input')
+
+//buttons text
+for(let button of buttons){
+    button.addEventListener('click',function(e){
+        let buttonText=e.target.innerText;
+        if(buttonText==='AC'){
+            input.value=''
+        }
+        else if(buttonText==='='){
+            try {
+                input.value=eval(input.value)
+            } catch (error) {
+                input.value='is ts tuff twin?'
+            }
+        }
+        else{
+            input.value+=buttonText;
+        }
+
+        setTimeout(()=>{
+            
+        },10000)
+    })
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
